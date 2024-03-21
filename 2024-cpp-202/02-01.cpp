6e25eb3be52a79428e9f2702887fc6f26a8134cd
gheun <gheun098@BOOK-FMEACV3VT8>
2024-03-21 11:45:24 +0900

    02-01 C++ 기본예제

---
+++ 2024-cpp-202/02-01.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+int main(void) {
+
+	// count: Ãâ·Â°´Ã¼, endl: °³Çà°´Ã¼
+	std::cout << "Hello World!" << std::endl;
+	return 0;
+}
--- 2024-cpp-202/연습예제.cpp
+++
@@ -1,7 +0,0 @@
-﻿#include <stdio.h>
-int main(void) {
-
-	// 주석 테스트
-	printf("Hello World");
-	return 0;
-}
