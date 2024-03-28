ec155ee3569fbc2a7a917e6742a49fb4838b65c0
gheun <gheun098@BOOK-FMEACV3VT8>
2024-03-28 13:12:26 +0900

    함수를 통해 배열값을 변환하자

---
+++ 2024-cpp-202/grammer/배열과 포인터 그리고 함수.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+// 이름공간
+using namespace std;
+
+void erase(int* ptr) {
+	for (int i = 0; i < length; i++) {
+		ptr[i] = 0;
+	}
+}
+
+int main(void) {
+	int arr[4] = {10, 20, 30, 40};
+	int length = sizeof(arr) / sizeof(arr[0]);
+	erase(arr);
+	for (int i = 0; i < sizeof(arr); i++) {
+		printf("%d ", arr[i]);
+	}
+}
