42782fabd3d789b393dd85251b3caeb55937f617
gheun <gheun098@BOOK-FMEACV3VT8>
2024-03-28 11:18:16 +0900

    포인터가 배열을 가리키는 원리

--- 2024-cpp-202/grammer/배열과 포인터.cpp
+++ 2024-cpp-202/grammer/배열과 포인터.cpp
@@ -3,9 +3,29 @@
 using namespace std;
 
 int main(void) {
-	int arr[4] = {1, 2, 3, 4};
+	int arr[4] = {10, 20, 30, 40};
 	char str[4] = "abc";
 
-	printf("%d / %d\n", &arr[0], &arr[1]);
-	printf("%d / %d", *(arr+0), *(arr+1));
+	//printf("%d / %d\n", &arr[0], &arr[1]);
+	//printf("%d / %d", *(arr+0), *(arr+1));
+
+	int i = 1;
+	char c = 'b';
+	double d = 3.0;
+
+	int* pi = &i;
+	char* pc = &c;
+	double* pd = &d;
+
+	//각각 4, 1, 8 차이
+	printf("%d %d %d\n", pi, pc, pd);
+	printf("%d %d %d\n", pi+1, pc+1, pd+1);
+	//printf("%d %d %d\n", sizeof(pi), sizeof(pc), sizeof(pd));
+
+	pi = arr;
+	printf("%d %d\n", pi + 1, arr + 1);
+	printf("%d %d\n", *(pi + 1), *(arr + 1));
+	printf("%d %d\n", pi[1], arr[1]);
+
+	return 0;
 }
