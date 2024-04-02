a9f78e978cccb42e770c4fd9dd745323f3bcfaf0
gheun <gheun098@BOOK-FMEACV3VT8>
2024-04-02 15:49:44 +0900

    call by reference

--- 2024-cpp-202/call-by-reference.cpp
+++ 2024-cpp-202/call-by-reference.cpp
@@ -3,7 +3,7 @@
 //이름공간. std를 안해도 된다
 using namespace std;
 
-void call_by_value(int* val)
+void call_by_reference(int* val)
 {
     //새로운 지역변수 val을 만들고 val가 가리키고 있는 대상의 값을 바꾼다.
     *val = 10;
@@ -12,7 +12,7 @@ int main(void)
 {
     int num = 4;
     //num의 값은 바뀌지 않는다.
-    call_by_value(&num);
+    call_by_reference(&num);
     cout << num;
     return 0;
 }
--- 2024-cpp-202/grammer/call-by-reference.cpp
+++ 2024-cpp-202/grammer/call-by-reference.cpp
@@ -3,16 +3,16 @@
 //이름공간. std를 안해도 된다
 using namespace std;
 
-void call_by_value(int* val)
+void call_by_reference(int* val)
 {
     //새로운 지역변수 val을 만들고 val가 가리키고 있는 대상의 값을 바꾼다.
-    *val = 10;
+    *al = 10;
 }
 int main(void)
 {
     int num = 4;
     //num의 값은 바뀌지 않는다.
-    call_by_value(&num);
+    call_by_reference(&num);
     cout << num;
     return 0;
 }
