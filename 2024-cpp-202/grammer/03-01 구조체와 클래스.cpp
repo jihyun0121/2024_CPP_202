588f340bae2897aa697a76a78ee34835a8134600
gheun <gheun098@BOOK-FMEACV3VT8>
2024-04-09 15:43:52 +0900

    클래스는 defualt가 private이다.

--- 2024-cpp-202/grammer/03-01 구조체와 클래스.cpp
+++ 2024-cpp-202/grammer/03-01 구조체와 클래스.cpp
@@ -3,16 +3,28 @@
 
 using namespace std;
 
-struct Student {
+//struct Student {
+//    int hakbun_;
+//    string name_;
+//};
+//
+//int main(void) {
+//    struct Student jisu;
+//    // 구조체는 defualt가 public이다.
+//    jisu.hakbun_ = 2202;
+//    jisu.name_ = "나지수";
+//    return 0;
+//}
+
+class Student {
     int hakbun_;
     string name_;
 };
 
 int main(void) {
     struct Student jisu;
-    // 구조체는 defualt가 public이다.
+    // 클래스는 defualt가 private이다.
     jisu.hakbun_ = 2202;
     jisu.name_ = "나지수";
     return 0;
 }
-
