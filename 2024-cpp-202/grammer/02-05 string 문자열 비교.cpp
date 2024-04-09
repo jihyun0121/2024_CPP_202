404ab08ffd016ff2fe8e873d77c7e7e558823ddf
gheun <gheun098@BOOK-FMEACV3VT8>
2024-04-09 14:55:16 +0900

    String 문자열 비교 (C++ 방식)

---
+++ 2024-cpp-202/grammer/02-05 string 문자열 비교.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void) {
+    string str = "c++";
+    if (str == "c++")
+        cout << str;
+    else
+        cout << "다른 문자열";
+}
