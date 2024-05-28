37cb94772c7f2a2f75dd6f5a0b42a3f5db64d6a4
gheun <gheun098@gmail.com>
2024-05-28 16:06:17 +0900

    05-01 부모/자식 생성/소멸자 호출 순서

--- 2024-cpp-202/grammer/05-01 상속 기본.cpp
+++ 2024-cpp-202/grammer/05-01 상속 기본.cpp
@@ -6,10 +6,15 @@ using namespace std;
 class Person {
 public:
     Person(string name, int age)
-        : name_(name), age_(age) {}
+        : name_(name), age_(age) {
+        cout << "부모 생성자 호출" << endl;
+    }
 
+    ~Person() {
+        cout << "부모 소멸자 호출" << endl;
+    }
     void show_person() {
-        cout << name << "의 나이는 " << age << "입니다." << endl;
+        cout << name_ << "의 나이는 " << age_ << "입니다." << endl;
     }
 
 private :
@@ -20,7 +25,10 @@ private :
 class Student : public Person {
 public:
     Student(string name, int age, int id) : Person(name, age) {
-
+        cout << "자식 생성자 호출" << endl;
+    }
+    ~Student() {
+        cout << "자식 소멸자 호출" << endl;
     }
 
     void show_person() {
@@ -36,5 +44,6 @@ int main(void) {
 
     Student* stu = new Student("BJH", 18, 2204);
     stu->show_person();
+    delete stu;
     return 0;
 }
