1ec03d2aa48231b797bc03a2a254e70e4f90e475
gheun <gheun098@gmail.com>
2024-08-20 15:50:26 +0900

    05-03 다형성 실습 예제

---
+++ 05-03 다형성.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Animal {
+public:
+    Animal(string name, unsigned int age, int leg_num)
+        : name_(name), age_(age), leg_num_(leg_num)
+    {}
+
+    void walk(void) {
+        cout << "°È´Ù." << endl;
+    }
+    void bark(void) {
+        cout << "Â¢´Ù." << endl;
+    }
+    void eat(void) {
+        cout << "¸Ô´Ù." << endl;
+
+    }
+
+private:
+    string name_;
+    unsigned int age_;
+    int leg_num_;
+};
+
+
+void main(void) {
+    Animal* animal = new Animal("¿ä¾ÆÁ¶ºñ", 8, 2);
+    animal->bark();
+    animal->eat();
+    animal->walk();
+    delete animal;
+
+}
