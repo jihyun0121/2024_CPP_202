01677b95937b5611806a1dfcce57798ef5ca2ce1
gheun <gheun098@gmail.com>
2024-09-10 15:01:23 +0900

    08-01 벡터 개념

---
+++ 2024-CPP-202/08-01 벡터.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void main(void) {
+	vector<long long> v;
+	v.push_back(4);
+	v.push_back(6);
+	v.push_back(4567);
+	v.push_back(632);
+	v.push_back(225);
+	v.push_back(4616164515);
+
+	cout << v[0] << endl;
+	cout << v.at(1) << endl;
+
+	cout << v[5] << endl;
+	v[5] = 50000000000;
+	cout << v[5] << endl;
+
+	for (int i = 0; i < v.size(); i++)
+	{
+		cout << v[i] << endl;
+	}
+
+	v.pop_back();
+	v.pop_back();
+
+	for (int i = 0; i < v.size(); i++)
+	{
+		cout << v[i] << endl;
+	}
+
+}
