e999fe564bcd087ec97f24952bdbc3638b31c35c
gheun <gheun098@gmail.com>
2024-10-17 10:29:26 +0900

    09-02 전통적인 반복문

---
+++ 2024-CPP-202/09-02 범위기반 for문.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void main(void) {
+
+	vector<int> v = { 455, 14, 27, 456, 23 };
+
+	for (int value : v)
+	{
+		cout << value << endl;
+	}
+
+}
